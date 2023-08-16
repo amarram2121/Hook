e8a40264d33450b24d40b83fc86a266729a4ac3d
Amar Ram <131468825+amarram2121@users.noreply.github.com>
2023-08-17 04:27:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import UseState from './Components/useState'
+// import UseEffect from './Components/useEffect'
+// import UseReducer from './Components/useReducer'
+import Todo from './Components/todo/todo'
+
+
+const App = () => {
+  return (
+    // <UseState/>
+    // <UseEffect/>
+    // <UseReducer/>
+    <Todo/>
+  )
+}
+
+export default App
